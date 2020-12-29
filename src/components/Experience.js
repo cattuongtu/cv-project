b81f1637e036923e490eff77b372b3662636c592
Cat-Tuong Tu <cattuongtu3@gmail.com>
2020-12-29 15:44:43 -0800

    Implemented Experience component with ability to add individual tasks.

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import uniqid from "uniqid";
 
 class Experience extends Component{
   constructor() {
@@ -11,18 +12,17 @@ class Experience extends Component{
         jobTitle: "Software Engineer",
         startDate: "October 2019",
         endDate: "January 2020",
-        tasks:["Created clothing website", "Maintained website database", 
-        "Communicated with clients with regards to technical questions"],
-      }],
+        tasks: ["Designed and implemented company website", "Managed store database", "Developed inventory software"],
+      },],
       experienceInput: {
         companyName: "",
         jobTitle: "",
         startDate: "",
         endDate: "",
-        tasks:[],
+        tasks: [],
       },
-      tasks: [],
-      editMode: false,
+      task: "",
+      editMode: true,
     }
   }
   
@@ -35,7 +35,7 @@ class Experience extends Component{
         jobTitle: prevState.jobTitle,
         startDate: prevState.startDate,
         endDate: prevState.endDate,
-        tasks: prevState.tasks,
+        tasks: prevState.tasks
       }
     });
   }
@@ -44,11 +44,11 @@ class Experience extends Component{
     const prevState = this.state.experienceInput;
     this.setState({
       experienceInput: {
-        companyName: prevState.value,
-        jobTitle: e.target.jobTitle,
+        companyName: prevState.companyName,
+        jobTitle: e.target.value,
         startDate: prevState.startDate,
         endDate: prevState.endDate,
-        tasks: prevState.tasks,
+        tasks: prevState.tasks
       }
     });
   }
@@ -57,11 +57,11 @@ class Experience extends Component{
     const prevState = this.state.experienceInput;
     this.setState({
       experienceInput: {
-        companyName: prevState.value,
+        companyName: prevState.companyName,
         jobTitle: prevState.jobTitle,
-        startDate: e.target.startDate,
+        startDate: e.target.value,
         endDate: prevState.endDate,
-        tasks: prevState.tasks,
+        tasks: prevState.tasks
       }
     });
   }
@@ -70,33 +70,47 @@ class Experience extends Component{
     const prevState = this.state.experienceInput;
     this.setState({
       experienceInput: {
-        companyName: prevState.value,
+        companyName: prevState.companyName,
         jobTitle: prevState.jobTitle,
         startDate: prevState.startDate,
         endDate: e.target.value,
-        tasks: prevState.tasks,
+        tasks: prevState.tasks
       }
     });
   }
 
-  handleAddTask = (e) => {
+  handleTaskChange = (e) => {
+    this.setState({
+      task: e.target.value,
+    });
+    console.log(e.target.value);
+  }
+
+  addTask = (e) => {
     e.preventDefault();
     const prevState = this.state.experienceInput;
     this.setState({
-
-    })
-
+        experienceInput: {
+          companyName: prevState.companyName,
+          jobTitle: prevState.jobTitle,
+          startDate: prevState.startDate,
+          endDate: prevState.endDate,
+          tasks: prevState.tasks.concat(this.state.task),
+        },
+        task: "" 
+    });
   }
 
   // Form submit function
   onSubmitInfo = (e) => {
     e.preventDefault();
     this.setState({
-      generalInfo: this.state.generalInfo.concat(this.state.generalInfoInput),
-      generalInfoInput: {
-        name: "",
-        email: "",
-        phoneNumber: ""
+      experience: this.state.experience.concat(this.state.experienceInput),
+      experienceInput: {
+        companyName: "",
+        jobTitle: "",
+        startDate: "",
+        endDate: "",
       },
       editMode: false,
     });
@@ -113,20 +127,85 @@ class Experience extends Component{
 
   // Function to remove 
   shiftSection = (e) => {
-    const prevState = this.state.generalInfo;
+    const prevState = this.state.experience;
     prevState.shift();
     e.preventDefault();
     this.setState({
-      generalInfo: prevState,
+      experience: prevState,
     })
   }
 
   render() {
+    if (!this.state.editMode) {
+      return ( 
+        <div>
+          <button onClick={this.switchMode}>Add Info</button>
+          <button onClick={this.shiftSection}>Remove Oldest Section</button>
+          {this.state.experience.map((section) => {
+            console.log(section);
+            return (
+              <div key={uniqid()}>
+                <h4 key={uniqid()}>{section.companyName}</h4>
+                <h4 key={uniqid()}>{section.jobTitle}</h4>
+                <h4 key={uniqid()}>{section.startDate}</h4>
+                <h4 key={uniqid()}>{section.endDate}</h4>
+                <ul>
+                  {section.tasks.map((task) => {
+                    return <li key={uniqid()}>{task}</li>
+                  })}
+                </ul>
+              </div>
+              
+            );
+          })}
+        </div>
+      );
+    }
+
     return ( 
       <div>
         <h1>
-          Experience Section
+          Experience
         </h1>
+        <form onSubmit={this.onSubmitInfo}>
+          <label htmlFor="companyNameInput"> Enter company name </label>
+          <input
+            onChange = {this.handleCompanyNameChange}
+            value = {this.state.experienceInput.companyName}
+            type = "text"
+            id="companyNameInput"
+          />
+          <label htmlFor="jobTitleInput"> Enter job title </label>
+          <input 
+            onChange = {this.handleJobTitleChange}
+            value = {this.state.experienceInput.jobTitle}
+            type = "text"
+            id = "jobTitleInput"
+          />
+          <label htmlFor="startDateInput"> Enter start date </label>
+          <input 
+            onChange = {this.handleStartDateChange}
+            value = {this.state.experienceInput.startDate}
+            type = "text"
+            id = "startDateInput"
+          />
+          <label htmlFor="endDateInput"> Enter end date </label>
+          <input 
+            onChange = {this.handleEndDateChange}
+            value = {this.state.experienceInput.endDate}
+            type = "text"
+            id = "endDateInput"
+          />
+          <label htmlFor="taskInput"> Enter task </label>
+          <input
+            onChange = {this.handleTaskChange}
+            type= "text"
+            id= "taskInput"
+          />
+          <button onClick={this.addTask}>Add Task</button>
+          <button type="submit">Add Information</button>
+          <button onClick={this.switchMode}>Cancel</button>
+        </form>
       </div>
     );
   }
